79ae32010b508a113b6b40ac936c732db6bee080
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2013-04-23 17:31:09 +0200

    funciones

--- Practica2/CRIP_P2/src/crip_p2/Funciones.java
+++ Practica2/CRIP_P2/src/crip_p2/Funciones.java
@@ -4,6 +4,8 @@
  */
 package crip_p2;
 
+import java.util.*;
+
 /**
  *
  * @author Carlos Basso
@@ -13,22 +15,23 @@ public class Funciones {
     public static Integer comprobarPeriocidad(String secuencia) {
         Integer longitudPeriodo = null;
         boolean iguales = true, salir = false;
-        
-        for (int grupo = 1; grupo <= secuencia.length()/2 && !salir; grupo++) {
+
+        for (int grupo = 1; grupo <= secuencia.length() / 2 && !salir; grupo++) {
             for (int indParPer = 0; indParPer < grupo && iguales; indParPer++) {
-                for (int indParCom = indParPer + grupo; indParCom < secuencia.length() && iguales; indParCom+=grupo) {
+                for (int indParCom = indParPer + grupo; indParCom < secuencia.length() && iguales; indParCom += grupo) {
                     if (secuencia.charAt(indParPer) != secuencia.charAt(indParCom)) {
                         iguales = false;
                     }
                 }
             }
-            if (!iguales) { iguales = true; }
-            else { 
+            if (!iguales) {
+                iguales = true;
+            } else {
                 salir = true;
                 longitudPeriodo = grupo;
             }
         }
-        
+
         return longitudPeriodo;
     }
 
@@ -46,15 +49,80 @@ public class Funciones {
             }
         }
         Integer suma = 0;
-        
+
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < sucesion.length(); j++) {
-                suma += polComp[j]*Integer.parseInt(String.valueOf(salida.charAt(i+j)));
+                suma += polComp[j] * Integer.parseInt(String.valueOf(salida.charAt(i + j)));
             }
-            salida = salida.concat(String.valueOf(suma%2));
+            salida = salida.concat(String.valueOf(suma % 2));
             suma = 0;
         }
-        
+
         return salida;
+    }
+
+    private static boolean postulado1(String sucesion) {
+        int unos = 0, ceros = 0;
+        boolean salida;
+        /*
+         * Primer Postulado
+         */
+        for (int i = 0; i < sucesion.length(); i++) {
+            if (sucesion.charAt(i) == '1') {
+                unos++;
+            } else if (sucesion.charAt(i) == '0') {
+                ceros++;
+            }
+        }
+        if (unos == ceros) {
+            salida = true;
+        } else if (unos - ceros == -1 || ceros - unos == -1) {
+            salida = true;
+        } else {
+            salida = false;
+        }
+        return salida;
+    }
+
+    private static boolean postulado2(String sucesion) {
+        boolean salida = false;
+        ArrayList<Integer> rachas = new ArrayList<Integer>();
+        int periodo;
+        periodo = comprobarPeriocidad(sucesion);
+        char simbolo = sucesion.charAt(0);
+        for (int j = 0, cont = 0; j < periodo; j++) {
+            if (j == 0) {
+                simbolo = sucesion.charAt(j);
+                cont++;
+            } else if (sucesion.charAt(j) == simbolo) {
+                simbolo = sucesion.charAt(j);
+                rachas.add(cont);
+                cont = 0;
+            }
+            if (sucesion.charAt(j) != simbolo) {
+                cont++;
+            }
+        }
+        Collections.sort(rachas);
+        if (rachas.size() / 2 == contar(rachas, 1)) {
+            salida = true;
+        }
+        return salida;
+    }
+
+    private static int contar(ArrayList<Integer> a, int num) {
+        int veces = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) == num) {
+                veces++;
+            }else{
+                break;
+            }
+        }
+
+        return veces;
+    }
+
+    public static void golomb(String sucesion) {
     }
 }
